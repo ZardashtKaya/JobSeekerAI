eec51b64d1f0a0eb284d72ef86a04a9912a2a20c
Zardasht Kaya <zardashtkaya@gmail.com>
2023-06-23 01:57:19 +0300

    fix prompts

--- backend/parser.py
+++ backend/parser.py
@@ -2,7 +2,7 @@ from reader import ResumeReader
 import openai
 import api
 
-openai.api_key =  api.__init__()
+api.__init__()
 
 class ResumeParser:
     def __init__(self, file):
@@ -12,18 +12,19 @@ class ResumeParser:
         self.text = ' '.join(self.text)
 
     def get_info(text):
-        completion = openai.Completion.create(model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": """You are are a resume reader, and you will respnd with only a score system, for each job classification, the output should be as follows:
-            First and Last Name: [name]
-            Email: [email]
-            Phone: [phone]
-            Address: [address]
-            [category]: [score in percentage from 0.0 to 1.0]]]]
-            for category rating don't include anything that isn't a number
-            """},
-            {"role": "user", "content": text}
-        ]
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": """You are are a resume reader, and you will respnd with only a score system, for each job classification, the output should be as follows:
+                First and Last Name: [name]
+                Email: [email]
+                Phone: [phone]
+                Address: [address]
+                [category]: [score in percentage from 0.0 to 1.0] examples such as videography, photography, etc.
+                for category rating don't include anything that isn't a number
+                """},
+                {"role": "user", "content": text.text}
+            ]
         )
 
         response = completion.choices[0].message.content.strip()
